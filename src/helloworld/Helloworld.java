651866b97efceefd1ce04026bbd573b74cbbfa9c
fcuD0610990 <47844967+fcuD0610990@users.noreply.github.com>
2019-02-21 15:01:38 +0800

    Update Helloworld.java

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class Helloworld {
     public static void main(String[] args) {
-    	System.out.println("Hello world");
+    	System.out.println("Hello OPP");
     }
 }
